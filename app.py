074d3be1c2647dd1d3e5bf1d348f2c77cd1011c3
salman <salmanraju1809@gmail.com>
2026-03-05 10:23:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import os
+import tempfile
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_classic.chains.retrieval_qa.base import RetrievalQA
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load API key
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+st.set_page_config(page_title="Multi PDF Research Assistant", page_icon="📚")
+
+st.title("📚 Multi-PDF Research Assistant (GenAI RAG)")
+st.write("Upload multiple PDFs and ask questions about them.")
+
+# Upload PDFs
+uploaded_files = st.file_uploader(
+    "Upload PDF files",
+    type="pdf",
+    accept_multiple_files=True
+)
+
+if uploaded_files:
+
+    documents = []
+
+    with st.spinner("Loading PDFs..."):
+
+        for uploaded_file in uploaded_files:
+
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                tmp_file.write(uploaded_file.read())
+                file_path = tmp_file.name
+
+            loader = PyPDFLoader(file_path)
+            docs = loader.load()
+            documents.extend(docs)
+
+    st.success("PDFs loaded successfully!")
+
+    # Split text
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    split_docs = text_splitter.split_documents(documents)
+
+    # Embeddings
+    embeddings = HuggingFaceEmbeddings()
+
+    # Vector DB
+    vectorstore = FAISS.from_documents(split_docs, embeddings)
+
+    retriever = vectorstore.as_retriever()
+
+    # Gemini LLM
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash",
+        google_api_key=GOOGLE_API_KEY
+    )
+
+    # RAG Chain
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=retriever
+    )
+
+    st.divider()
+
+    question = st.text_input("Ask a question about the PDFs")
+
+    if question:
+
+        with st.spinner("Searching documents..."):
+            response = qa_chain.run(question)
+
+        st.subheader("Answer")
+        st.write(response)
